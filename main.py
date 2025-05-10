c43ee8512497c8c69152a23f91e659f4860a7c00
40325955 <ehaw01@qub.ac.uk>
2025-05-10 13:23:08 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -6,4 +6,6 @@
 #Have a web scraping tool which will allow the gathering of information
 #Make it give different information at different times, before market open, close etc
 #Have it come through to my phone
-#Sources - Quiver Quantitive, Etoro Wallets, Trading News Outlets, Warren Buffet, BlackRock wallets etc.
+#Sources - Quiver Quantitative, E-toro Wallets, Trading News Outlets, Warren Buffet, BlackRock wallets etc.
+#It has knowledge of what is in your portfolio
+#It will eventually trade on your behalf
